1dbcfd5f34ab9b10161f27e7a56d69c4b26e3d55
AgitaDumpling <agitaliene.berzina@gmail.com>
2022-10-18 09:37:25 +0300

    Beidzot progress
    WEEEEE

--- funkcijas.js
+++ funkcijas.js
@@ -1,6 +1,12 @@
 function paradies(){
  document.getElementById("vieta").style.display="block"
 }
-function close(){
+function closethis(){
     document.getElementById("vieta").style.display="none"
+}
+function paradies1(){
+    document.getElementById("vieta1").style.display="block"
+   }
+   function closethis1(){
+    document.getElementById("vieta1").style.display="none"
 }
