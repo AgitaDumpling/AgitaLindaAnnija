fd253ca6a1e6e16e79c3fd5d793d29f3b1b163a2
AgitaDumpling <agitaliene.berzina@gmail.com>
2022-10-18 17:55:10 +0300

    Ieliktas reālās bildes un teksts
    Puse no visa

--- funkcijas.js
+++ funkcijas.js
@@ -10,3 +10,45 @@ function paradies1(){
    function closethis1(){
     document.getElementById("vieta1").style.display="none"
 }
+function paradies2(){
+    document.getElementById("vieta2").style.display="block"
+   }
+   function closethis2(){
+    document.getElementById("vieta2").style.display="none"
+}
+function paradies3(){
+    document.getElementById("vieta3").style.display="block"
+   }
+   function closethis3(){
+    document.getElementById("vieta3").style.display="none"
+}
+function paradies4(){
+    document.getElementById("vieta4").style.display="block"
+   }
+   function closethis4(){
+    document.getElementById("vieta4").style.display="none"
+}
+function paradies5(){
+    document.getElementById("vieta5").style.display="block"
+   }
+   function closethis5(){
+    document.getElementById("vieta5").style.display="none"
+}
+function paradies6(){
+    document.getElementById("vieta6").style.display="block"
+   }
+   function closethis6(){
+    document.getElementById("vieta6").style.display="none"
+}
+function paradies7(){
+    document.getElementById("vieta7").style.display="block"
+   }
+   function closethis7(){
+    document.getElementById("vieta7").style.display="none"
+}
+function paradies8(){
+    document.getElementById("vieta8").style.display="block"
+   }
+   function closethis8(){
+    document.getElementById("vieta8").style.display="none"
+}
